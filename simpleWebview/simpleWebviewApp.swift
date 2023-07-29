3e5390b5fea480ae3557d114853a5c3d458bb5f0
jfernandez <jfernandezc@caixabanktech.com>
2023-07-29 20:02:41 +0200

    Initial Commit

---
+++ simpleWebview/simpleWebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleWebviewApp.swift
+//  simpleWebview
+//
+//  Created by Julio Fernández on 29/7/23.
+//
+
+import SwiftUI
+
+@main
+struct simpleWebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
